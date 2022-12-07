d088bce1aa07c2c14ca7d2d94d8c486116c488ac
tusharvibhakar03 <tvibhaker@gmail.com>
2022-12-07 08:42:58 +0530

    TUTMVN

---
+++ src/test/java/com/tutorial/demo/testsuite/MyAccountTest.java
@@ -0,0 +1,68 @@
+package com.tutorial.demo.testsuite;
+
+import com.tutorial.demo.pages.MyAccount;
+import com.tutorial.demo.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyAccountTest extends BaseTest {
+
+    MyAccount myAccount = new MyAccount();
+
+    //MyAccount
+    @Test
+    public void verifyUserShouldNavigateToRegisterPageSuccessfully() {
+        myAccount.clickOnMyAccount();
+        myAccount.selectMyAccountOptions("Register");
+        Assert.assertEquals(myAccount.getVerifyRegister(), "Register Account", "Error Message not displayed");
+    }
+
+    @Test
+    public void verifyUserShouldNavigateToLoginPageSuccessfully() {
+        myAccount.clickOnMyAccount();
+        myAccount.selectMyAccountOptions("Login");
+        Assert.assertEquals(myAccount.getVerifyReturning(), "Returning Customer", "Error Message not displayed");
+    }
+
+    @Test
+    public void verifyThatUserRegisterAccountSuccessfully() {
+        myAccount.clickOnMyAccount();
+        myAccount.selectMyAccountOptions("Register");
+
+        myAccount.enterFirstName("Tushar");
+        myAccount.enterLastName("Vibhakar");
+        myAccount.enterEmailAdd("tv@gmail.com");
+        myAccount.enterPhone("Indra112");
+        myAccount.enterPassword("012345678");
+        myAccount.enterConfirmPass("012345678");
+
+        myAccount.clickYesonNewsLetter();
+        myAccount.clickOnPrivacy();
+        myAccount.clickFinalContinue();
+
+        Assert.assertEquals(myAccount.getVerifyMessage(), "Your Account Has Been Created!", "Error Message not displayed");
+        myAccount.clickOnContinueButton();
+
+        myAccount.clickOnMyAccountTab();
+        myAccount.selectMyAccountOptions("Logout");
+        Assert.assertEquals(myAccount.getVerifyLogout(), "Account Logout", "Error Message not displayed");
+        myAccount.clickOnContinueTab();
+    }
+
+    @Test
+    public void verifyThatUserShouldLoginAndLogoutSuccessfully() {
+        myAccount.clickOnMyAccountTab();
+        myAccount.selectMyAccountOptions("Login");
+
+        myAccount.enterEmailAdd("tv@gmail.com");
+        myAccount.enterPassword("Indra112");
+
+
+        myAccount.clickOnMyAccountTab();
+        myAccount.selectMyAccountOptions("Logout");
+        //Assert.assertEquals(myAccount.getVerifyLogout(), "Account Logout", "Error Message not displayed");
+        myAccount.clickOnContinueTab();
+    }
+
+
+}
